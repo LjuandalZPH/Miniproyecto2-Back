c5aa190b7a7c8adcbec66f0240f15e39119adb27
David Pinto's <juan.david.pinto@correounivalle.edu.co>
2025-10-20 19:34:39 -0500

    Initialize Express server with middleware and routes
    Sets up the main entry point for the Express server, including middleware, routes, and database connection.

---
+++ server.ts
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview Punto de entrada principal del servidor Express.
+ * Configura middlewares, rutas y realiza la conexión a la base de datos.
+ * 
+ * @module server
+ */
+import dotenv from "dotenv";
+import express from "express";
+import router from "./api/routes/routes";
+import connectDB from "./api/config/database"; // Importa la función de conexión
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+import cors from "cors";
+
+// allow petitions from frontend
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
+
+/**
+ * Middleware para parsear JSON en las peticiones.
+ */
+app.use(express.json());
+
+/**
+ * Ruta principal de prueba.
+ */
+app.get("/", (req, res) => {
+  res.send("Everything its working, Rejoice PEASANTS");
+});
+
+/**
+ * Rutas de la API agrupadas bajo /api.
+ */
+app.use("/api", router);
+
+/**
+ * Conexión a la base de datos MongoDB Atlas.
+ */
+connectDB();
+
+/**
+ * Inicia el servidor en el puerto definido.
+ */
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+	
